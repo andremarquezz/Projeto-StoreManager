c39f927d97f63c5e13688754c899adc5131165ac
Jey <andre.marqquez@gmail.com>
2022-08-11 15:22:08 -0300

    Create productsModel.js
    create first query to communicate with database

---
+++ models/productsModel.js
@@ -0,0 +1,11 @@
+const connection = require('./connection');
+
+const getAll = async () => {
+  const query = 'SELECT * FROM StoreManager.products';
+  const [data] = await connection.execute(query);
+  return data;
+};
+
+module.exports = {
+  getAll,
+};
