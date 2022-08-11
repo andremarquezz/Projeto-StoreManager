3c803178b79b948aa175407a0f63bfd0563abcd6
Jey <andre.marqquez@gmail.com>
2022-08-11 15:20:02 -0300

    delete comments
    delete comments that came with the project

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const express = require('express');
 
 const app = express();
 
-// não remova esse endpoint, é para o avaliador funcionar
+app.use(express.json());
+
+const productsController = require('./controllers/productsController');
+
 app.get('/', (_request, response) => {
   response.send();
 });
 
-// não remova essa exportação, é para o avaliador funcionar
-// você pode registrar suas rotas normalmente, como o exemplo acima
-// você deve usar o arquivo index.js para executar sua aplicação 
+app.use('/products', productsController);
+
 module.exports = app;
