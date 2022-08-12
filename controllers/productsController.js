46da3f2482309ee4877b5d5c1a73b2058972b62c
Jey <andre.marqquez@gmail.com>
2022-08-12 13:28:33 -0300

    feat: adiciona rota para registro de produtos
    adiciona rota que chama Product Services para registro de produto e retorna status 201 e o produto com id

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -3,10 +3,17 @@ const ProductServices = require('../services/productsService');
 
 const router = express.Router();
 
-router.get('/', async (_req, res) => {
-  const products = await ProductServices.getAll();
-  return res.status(products.code).json(products.data);
-});
+router
+  .route('/')
+  .get(async (_req, res) => {
+    const products = await ProductServices.getAll();
+    return res.status(products.code).json(products.data);
+  })
+  .post(async (req, res) => {
+    const { name } = req.body;
+    const registeredProduct = await ProductServices.registerProduct(name);
+    return res.status(registeredProduct.code).json(registeredProduct.data);
+  });
 
 router.get('/:id', async (req, res) => {
   const { id } = req.params;
