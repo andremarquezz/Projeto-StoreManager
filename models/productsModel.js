50e2812156cc380950116b864ef9fce3d6fda21c
Jey <andre.marqquez@gmail.com>
2022-08-12 13:27:06 -0300

    feat: função que registra produto no DB
    Adiciona função que registra o produto no DB e retorna o mesmo na camada Model

--- models/productsModel.js
+++ models/productsModel.js
@@ -7,15 +7,30 @@ const getAll = async () => {
 };
 
 const getOne = async (id) => {
-  const query = (`
+  const query = `
   SELECT * FROM StoreManager.products
   WHERE id = ?
-  `);
+  `;
   const [[data]] = await connection.execute(query, [id]);
   return data;
+};
+
+const registerProduct = async (name) => {
+  const query = `
+  INSERT INTO StoreManager.products(name)
+  VALUE (?)
+  `;
+  await connection.execute(query, [name]);
+  const querySelectProduct = `
+  SELECT * FROM StoreManager.products
+  WHERE name = ?
+  `;
+  const [[product]] = await connection.execute(querySelectProduct, [name]);
+  return product;
 };
 
 module.exports = {
   getAll,
   getOne,
+  registerProduct,
 };
