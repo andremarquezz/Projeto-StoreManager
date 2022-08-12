74db9bf5d61d3d22d02bacad2d7d5b134ad076ac
Jey <andre.marqquez@gmail.com>
2022-08-12 17:47:55 -0300

    feat: middleware que valida informações
    Adiciona middleware que valida informações enviadas no corpo da requisição

---
+++ middlewares/validInfoSales.js
@@ -0,0 +1,30 @@
+const joi = require('joi');
+const { CustomErrors } = require('../errors/CustomError');
+
+const schemaInfoSales = joi.object({
+  productId: joi.required(),
+  quantity: joi.number().min(1).required(),
+});
+
+const validInfoSales = (req, _res, next) => {
+  const infosProducts = req.body;
+  infosProducts.map((infoProduct) => {
+    const validate = schemaInfoSales.validate(infoProduct);
+    const { error } = validate;
+    if (error) {
+      const messageError = error.details[0].message;
+      const typeError = error.details[0].type;
+      switch (typeError) {
+        case 'any.required':
+          throw new CustomErrors(messageError, 400);
+        case 'number.min':
+          throw new CustomErrors(messageError, 422);
+        default:
+          throw new CustomErrors(messageError, 500);
+      }
+    }
+    return next();
+  });
+};
+
+module.exports = validInfoSales;
