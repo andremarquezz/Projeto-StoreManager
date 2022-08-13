7a7b8eb574383c3b57c6337184a9cbb976c71ed7
Jey <andre.marqquez@gmail.com>
2022-08-12 21:24:15 -0300

    atualiza pr para avaliador

---
+++ tests/unit/controllers/salesController.test.js
@@ -0,0 +1,6 @@
+const sinon = require("sinon");
+// const { expect } = require("chai");
+
+describe("Testa a camada Sales Controller", () => {
+  beforeEach(sinon.restore);
+});
---
+++ tests/unit/services/salesServices.test.js
@@ -0,0 +1,6 @@
+const sinon = require("sinon");
+// const { expect } = require("chai");
+
+describe("Testa a camada Sales Service", () => {
+  beforeEach(sinon.restore);
+});
