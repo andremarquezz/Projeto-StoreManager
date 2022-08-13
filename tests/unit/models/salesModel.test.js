f90999b4906fb0f7fc518f15cb33ddf60e6ae2d2
Jey <andre.marqquez@gmail.com>
2022-08-13 10:17:26 -0300

    feat: testes para a camada Sales Model
    Adiciona camada de testes sales model  e primeiro teste que checa se o produto existe

---
+++ tests/unit/models/salesModel.test.js
@@ -0,0 +1,24 @@
+const sinon = require("sinon");
+const chai = require("chai");
+const { expect } = chai;
+const chaiAsPromised = require("chai-as-promised");
+const SalesModel = require("../../../models/salesModel");
+const connection = require("../../../models/connection");
+
+chai.use(chaiAsPromised);
+
+describe("Testa a camada Sales Model", () => {
+  beforeEach(sinon.restore);
+  it("Testa a chamada para checkProductExists", async () => {
+    const fakeResponse = {
+      exists: 1,
+    };
+
+    sinon.stub(connection, "execute").resolves([[fakeResponse]]);
+
+    return expect(SalesModel.checkProductExists(1)).to.be.eventually.deep.eq(
+      fakeResponse
+    );
+  });
+  
+});
